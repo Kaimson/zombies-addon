f16a9d9250efc6e585c9a76888add2a012a519c2
Kaimson <julien.rheinbach@optimal.de>
2025-03-26 21:33:09 +0100

    fix: dont overwrite method unnecessarily

--- src/main/java/kr/hahaha98757/zombiesaddon/mixins/MixinModelPlayer.java
+++ src/main/java/kr/hahaha98757/zombiesaddon/mixins/MixinModelPlayer.java
@@ -10,48 +10,29 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ModelPlayer.class)
 public abstract class MixinModelPlayer extends MixinModelBiped {
-    @Shadow
-    public ModelRenderer bipedLeftLegwear;
-    @Shadow
-    public ModelRenderer bipedRightLegwear;
-    @Shadow
-    public ModelRenderer bipedLeftArmwear;
-    @Shadow
-    public ModelRenderer bipedRightArmwear;
-    @Shadow
-    public ModelRenderer bipedBodyWear;
-
-    @Override
-    public void render(Entity entityIn, float f, float g, float h, float i, float j, float scale) {
-        super.render(entityIn, f, g, h, i, j, scale);
-        GlStateManager.pushMatrix();
-        if(ZombiesAddonConfig.isEnableMod() && PlayerVisibility.playerVisibility && ZombiesAddonConfig.isPlayerTranslucent() && PlayerVisibility.isPlayerInvisible(entityIn)) {
+    @Inject(method = "render", at = @At("HEAD"))
+    private void za$preRender(Entity entityIn, float f, float g, float h, float i, float j, float scale, CallbackInfo ci) {
+        if (ZombiesAddonConfig.isEnableMod() && PlayerVisibility.playerVisibility && ZombiesAddonConfig.isPlayerTranslucent() && PlayerVisibility.isPlayerInvisible(entityIn)) {
             GlStateManager.color(1.0F, 1.0F, 1.0F, PlayerVisibility.getAlpha(entityIn));
             GlStateManager.depthMask(false);
             GlStateManager.enableBlend();
             GlStateManager.blendFunc(770, 771);
             GlStateManager.alphaFunc(516, 0.003921569F);
         }
-        if (this.isChild) {
-            GlStateManager.scale(0.5F, 0.5F, 0.5F);
-            GlStateManager.translate(0.0F, 24.0F * scale, 0.0F);
-        } else {
-            if (entityIn.isSneaking()) GlStateManager.translate(0.0F, 0.2F, 0.0F);
-        }
-        this.bipedLeftLegwear.render(scale);
-        this.bipedRightLegwear.render(scale);
-        this.bipedLeftArmwear.render(scale);
-        this.bipedRightArmwear.render(scale);
-        this.bipedBodyWear.render(scale);
+    }
 
-        if(ZombiesAddonConfig.isEnableMod() && PlayerVisibility.playerVisibility && ZombiesAddonConfig.isPlayerTranslucent() && PlayerVisibility.isPlayerInvisible(entityIn)) {
+    @Inject(method = "render", at = @At("TAIL"))
+    private void za$postRender(Entity entityIn, float f, float g, float h, float i, float j, float scale, CallbackInfo ci) {
+        if (ZombiesAddonConfig.isEnableMod() && PlayerVisibility.playerVisibility && ZombiesAddonConfig.isPlayerTranslucent() && PlayerVisibility.isPlayerInvisible(entityIn)) {
             GlStateManager.disableBlend();
             GlStateManager.alphaFunc(516, 0.1F);
             GlStateManager.depthMask(true);
         }
-        GlStateManager.popMatrix();
     }
 }
